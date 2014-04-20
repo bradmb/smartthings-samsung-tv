918764aae5cc440950bf969437baee131b287c62
Brad Butner <bradbutner83@gmail.com>
2014-04-20 00:58:40 -0700

    Fixed formatting

--- samsung.smartapp.groovy
+++ samsung.smartapp.groovy
@@ -14,24 +14,24 @@
  *  TV under Menu > Network > Network Status.
  */
 preferences {
-	section("Samsung TV Settings") {
-		input "settingIpAddress", "text", title: "IP Address", required: true
-		input "settingMacAddress", "text", title: "MAC Address", required: true
-    input "tvCommand", "enum", title: "Perform This Command", metadata:[values:["POWEROFF","POWERON","AV1","AV2","AV3","CLOCK_DISPLAY","COMPONENT1", "COMPONENT2", "HDMI", "HDMI1", "HDM2", "HDM3", "HDMI4", "INFO", "SLEEP"]], required: true
-	}
+    section("Samsung TV Settings") {
+        input "settingIpAddress", "text", title: "IP Address", required: true
+        input "settingMacAddress", "text", title: "MAC Address", required: true
+        input "tvCommand", "enum", title: "Perform This Command", metadata:[values:["POWEROFF","POWERON","AV1","AV2","AV3","CLOCK_DISPLAY","COMPONENT1", "COMPONENT2", "HDMI", "HDMI1", "HDM2", "HDM3", "HDMI4", "INFO", "SLEEP"]], required: true
+    }
 }
 
 def installed() {
-	log.debug "Installed with settings: ${settings}"
-	initialize()
+    log.debug "Installed with settings: ${settings}"
+    initialize()
 }
 
 
 def updated() {
-	log.debug "Updated with settings: ${settings}"
+    log.debug "Updated with settings: ${settings}"
 
-	unsubscribe()
-	initialize()
+    unsubscribe()
+    initialize()
 }
 
 def initialize() {
@@ -42,21 +42,21 @@ def initialize() {
 }
 
 def changedLocationMode(evt) {
-	log.debug "changedLocationMode: $evt"
-	tvAction(tvCommand)
+    log.debug "changedLocationMode: $evt"
+    tvAction(tvCommand)
 }
 
 def appTouch(evt) {
-	log.debug "appTouch: $evt"
-	tvAction(tvCommand)
+    log.debug "appTouch: $evt"
+    tvAction(tvCommand)
 }
 
 def parse(event) {
-	log.debug "${event}"
+    log.debug "${event}"
 }
 
 private tvAction(key) {
-	log.debug "Executing ${tvCommand}"
+    log.debug "Executing ${tvCommand}"
 
     // Standard Connection Data
     def appString = "iphone..iapp.samsung"
@@ -77,14 +77,14 @@ private tvAction(key) {
     def macAddress = settingMacAddress.replaceAll(":","").encodeAsBase64().toString()
     def macAddressLength = macAddress.getBytes().size()
 
-	// The Authentication Message
+    // The Authentication Message
     def authenticationMessage = "${(char)0x64}${(char)0x00}${(char)ipAddressLength}${(char)0x00}${ipAddress}${(char)macAddressLength}${(char)0x00}${macAddress}${(char)remoteNameLength}${(char)0x00}${remoteName}"
-	def authenticationMessageLength = authenticationMessage.getBytes().size()
+    def authenticationMessageLength = authenticationMessage.getBytes().size()
     
     def authenticationPacket = "${(char)0x00}${(char)appStringLength}${(char)0x00}${appString}${(char)authenticationMessageLength}${(char)0x00}${authenticationMessage}"
 
-	// If our initial run, just send the authentication packet so the prompt appears on screen
-	if (key == "AUTHENTICATE") {
+    // If our initial run, just send the authentication packet so the prompt appears on screen
+    if (key == "AUTHENTICATE") {
 	    sendHubCommand(new physicalgraph.device.HubAction(authenticationPacket, physicalgraph.device.Protocol.LAN, "${ipAddressHex}:D6D8"))
     } else {
         // Build the command we will send to the Samsung TV
